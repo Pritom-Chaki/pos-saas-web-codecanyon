0dada7355d53bdedb22fb2571404c447fadd6f4b
Pritom Chaki <pritom.cse.diu@gmail.com>
2024-09-14 22:35:24 +0600

    Update sales_invoice_pdf.dart

--- lib/PDF/sales_invoice_pdf.dart
+++ lib/PDF/sales_invoice_pdf.dart
@@ -63,7 +63,7 @@ FutureOr<Uint8List> generateSaleDocument58mm({required SaleTransactionModel tran
     pw.MultiPage(
       // pageFormat: PdfPageFormat.roll80,
       pageFormat: PdfPageFormat.letter.copyWith(width: 150, height:  getHeight(transactions)),
-          margin: pw.EdgeInsets.only(left: 5,right : 5, bottom: 20, top: 0),
+          margin: pw.EdgeInsets.only(left: 10,right : 5, bottom: 20, top: 0),
       crossAxisAlignment: pw.CrossAxisAlignment.start,
       header: (pw.Context context) {
         return pw.Column(
